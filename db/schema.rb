92b3826c59be08f7ccae161c17fe272672770a33
ally hyun <allyunhahyun@gmail.com>
2021-08-30 17:40:24 +0200

    migration updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_30_125920) do
+ActiveRecord::Schema.define(version: 2021_08_30_153733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 2021_08_30_125920) do
     t.boolean "admin"
     t.string "username"
     t.bigint "daycare_id"
+    t.string "phone_number"
     t.index ["daycare_id"], name: "index_users_on_daycare_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
