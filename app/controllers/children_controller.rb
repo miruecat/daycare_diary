8977ffac6d05fcfe399d66fe2a7c309b75a1a387
ally hyun <allyunhahyun@gmail.com>
2021-08-30 15:49:31 +0200

    controller skeleton done

---
+++ app/controllers/children_controller.rb
@@ -0,0 +1,35 @@
+class ChildrenController < ApplicationController
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def child_params
+
+  end
+end
