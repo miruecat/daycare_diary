ddd7e26f51d7e7fe8da700b225a0e89b598079c1
Sander Boogaards <sanderboogaards@gmail.com>
2021-09-01 17:14:13 +0200

    Added activity overview and children per parent on show page

--- app/models/child.rb
+++ app/models/child.rb
@@ -3,6 +3,7 @@ class Child < ApplicationRecord
   has_many :parenthoods
   has_many :parents, through: :parenthoods, class_name: "User"
   has_many :activities
+  has_one_attached :profile_picture
 
   validates :first_name, :last_name, :birthdate, :daycare_id, presence: true
   # Add validates that first_name / last_name / birthdate combination must be unique
