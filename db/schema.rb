cc8cc6daea02c0122e4e1bc194c9701c50ba726f
Sander Boogaards <sanderboogaards@gmail.com>
2021-09-02 15:08:56 +0200

    Date select added to child show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2021_09_01_130918) do
-=======
 ActiveRecord::Schema.define(version: 2021_09_02_085917) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
