206873ebe8d394b3f06fdcef5a8865cc9babd831
ally hyun <allyunhahyun@gmail.com>
2021-09-06 12:01:54 +0200

    merge new

--- app/controllers/parenthoods_controller.rb
+++ app/controllers/parenthoods_controller.rb
@@ -1,14 +1,17 @@
 class ParenthoodsController < ApplicationController
   def create
+    authorize @parenthood
     @parenthood = Parenthood.new(parenthood_params)
     if @parenthood.save
       redirect_to child_path(@child)
+    else
+      redirect_to children_path
     end
   end
 
   private
 
   def parenthood_params
-    params.permit(:parenthood).requires(:child_id, :user_id)
+    params.require(:parenthood).permit(:child_id, :user_id)
   end
 end
