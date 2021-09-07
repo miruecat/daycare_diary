a6e6aa2253f8eecd8fac9d2da65cb8f81f442560
Ilja Thoenes <iljathoenes@gmail.com>
2021-09-07 15:49:49 +0200

    aargh

--- app/javascript/controllers/children.js
+++ app/javascript/controllers/children.js
@@ -1,12 +1,18 @@
-let ids = [];
-$(".child_card").on("click", function(){
-  // If the id is not already in the array, add it. If it is, remove it
-  ids.indexOf(this.id) === -1 ? ids.push(this.id) : ids.splice(ids.indexOf(this.id),1);
-  $('input').val(ids.join(", ")); // populate the input with the array items separated with a comma
-});
+const { Modal } = require("bootstrap");
 
 document.querySelectorAll(".child_card").forEach((card) => {
     card.addEventListener("click", (event) => {
       event.currentTarget.classList.toggle(".imgback");
     });
   });
+  
+  //be able to select all of the inputs of a specific form
+  //foodinput on all the checks of the food Modal, give a common class
+  //play 
+  //health 
+  //sleep
+
+  //each (food, health et) submit button needs a unique class for the queryselector
+  
+  //when totalinput == false, submit_button displays none
+  //when input == true, submit_button displays
